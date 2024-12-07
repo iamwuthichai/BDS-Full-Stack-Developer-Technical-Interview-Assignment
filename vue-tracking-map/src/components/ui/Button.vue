817678c6687fb1ab89a2f48eeb134c7402c6b137
Wuthichai1996 <wuthichai.chuichaeng@gmail.com>
2024-12-07 19:09:58 +0700

    no message

--- vue-tracking-map/src/components/ui/Button.vue
+++ vue-tracking-map/src/components/ui/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="buttonClass">
+  <button :class="buttonClass" :disabled="disabled">
     <slot>
 
     </slot>
@@ -22,6 +22,10 @@ export default defineComponent({
     },
     buttonClass: {
       type: String,
+      required: true
+    },
+    disabled: {
+      type: Boolean,
       required: true
     },
   }
--- vue-tracking-map/src/views/MapSearchSiteView.vue
+++ vue-tracking-map/src/views/MapSearchSiteView.vue
@@ -64,35 +64,35 @@
 
         </div>
 
-      <hr class="my-2 w-full">
-      <div class="container">
-        <RouterLink :class="buttonClass"
-          :to="{
-            name: 'confirm_and_navigate',
-            query: {
-              latitude: siteInfo.latitude,
-              longitude: siteInfo.longitude,
-              location_name: siteInfo.site_name,
-              address: siteInfo.site_address,
-              contact: siteInfo.site_tel,
-              open_time: siteInfo.site_open_time,
-              close_time: siteInfo.site_close_time,
-            }
-          }">
-        <Button class="flex">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 sm:w-6 sm:h-6 mr-2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
-          </svg>
-          แผนที่สาขา
-        </Button>
-        </RouterLink>
-        <ButtonLink :class="buttonLinkClass">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 sm:w-6 sm:h-6 mr-2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
-          </svg>
-          โทร: {{ siteInfo.site_tel }}
-        </ButtonLink>
-      </div>
+        <hr class="my-2 w-full">
+        <div class="container">
+          <RouterLink :class="buttonClass"
+            :to="{
+              name: 'confirm_and_navigate',
+              query: {
+                latitude: siteInfo.latitude,
+                longitude: siteInfo.longitude,
+                location_name: siteInfo.site_name,
+                address: siteInfo.site_address,
+                contact: siteInfo.site_tel,
+                open_time: siteInfo.site_open_time,
+                close_time: siteInfo.site_close_time,
+              }
+            }">
+          <Button class="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 sm:w-6 sm:h-6 mr-2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
+            </svg>
+            แผนที่สาขา
+          </Button>
+          </RouterLink>
+          <ButtonLink :class="buttonLinkClass">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 sm:w-6 sm:h-6 mr-2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+            </svg>
+            โทร: {{ siteInfo.site_tel }}
+          </ButtonLink>
+        </div>
 
       </div>
     </div>
